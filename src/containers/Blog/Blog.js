02cf50eb38bca5f3d268639473ac53794149f4d0
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-26 05:21:45 +0530

    loaded full post

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,42 +1,46 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
 
 class Blog extends Component {
-
     state = {
-        posts: []
-    }
+        posts: [],
+        selectedPostId: null
+    };
 
     componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-            const posts = response.data.slice(0, 4);
-            const updatedPosts = posts.map(post => {
-                return {
-                    ...post,
-                    author: 'Rahul'
-                }
-            });
-            this.setState({posts: updatedPosts});
-        }) 
-        .catch() 
+        axios
+            .get("https://jsonplaceholder.typicode.com/posts")
+            .then((response) => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map((post) => {
+                    return {
+                        ...post,
+                        author: "Rahul",
+                    };
+                });
+                this.setState({ posts: updatedPosts });
+            })
+            .catch();
+    }
+    postSelectedHandler = id => {
+        this.setState({selectedPostId: id})
     }
 
-    render () {
-        const posts = this.state.posts.map(post => {
-            return <Post key={post.id} title={post.title} author={post.author} />
+    render() {
+        const posts = this.state.posts.map((post) => {
+            return (
+                <Post key={post.id} title={post.title} author={post.author} clicked={() => this.postSelectedHandler(post.id)} />
+            );
         });
         return (
             <div>
-                <section className="Posts">
-                    {posts}
-                </section>
+                <section className="Posts">{posts}</section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
