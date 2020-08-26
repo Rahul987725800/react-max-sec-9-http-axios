5ced99ea70852af0da9a64ed37f8b9728caaac67
Rahul Gupta <guptarahul70322@gmail.com>
2020-08-26 07:35:58 +0530

    default config axios

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import axios from 'axios'
+// global config
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
+axios.defaults.headers.common['Authorization'] = 'AUTH TOKEN';
+axios.defaults.headers.post['Content-Type'] = 'application/json'; 
+// Content-Type for  post request will be application/json
+// although application/json is default
+
 // affect all requests sent from the app
 axios.interceptors.request.use(request => {
     console.log(request)
